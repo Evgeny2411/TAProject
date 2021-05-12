fe8d7da133d7df8eef5444b0075972d61fd4ed2c
Evgeny Borisenko <borisenko1315@gmail.com>
2021-05-12 20:26:34 +0300

    Add path to DB

--- TAProject/src/TAProject/Main.java
+++ TAProject/src/TAProject/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Reading.readFile("disease.txt");
-        Reading.readFile("medicine.txt");
+        Reading.readFile("TAProject/disease.txt");
+        Reading.readFile("TAProject/disease.txt");
 
         System.out.println(DiseaseModel.getInstance().getModel().toString());
         System.out.println(MedicineModel.getInstance().getModel().toString());
