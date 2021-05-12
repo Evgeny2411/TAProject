242579fd3fda8c9a7a8d1f0bdfb0984ac31ed7b6
ProttoDem <ia03nelepin.dmitriy@gmail.com>
2021-05-12 20:37:02 +0300

    check for commit

--- TAProject/src/TAProject/Main.java
+++ TAProject/src/TAProject/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         Reading.readFile("TAProject/disease.txt");
-        Reading.readFile("TAProject/disease.txt");
+        Reading.readFile("TAProject/medicine.txt");
         //Denis lox ksta
         System.out.println(DiseaseModel.getInstance().getModel().toString());
         System.out.println(MedicineModel.getInstance().getModel().toString());
