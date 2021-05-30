7b60f38aa44259033355a824aa113a141ef3bba1
Logvynskyy <shimon511bm@gmail.com>
2021-05-30 23:13:41 +0300

    Вот вам ваше ДП

--- TAProject/src/TAProject/Main.java
+++ TAProject/src/TAProject/Main.java
@@ -3,15 +3,22 @@ package TAProject;
 import TAProject.BD.DiseaseModel;
 import TAProject.BD.MedicineModel;
 import TAProject.Methods.Reading;
+import TAProject.Methods.WhichDisease;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Reading.readFile("TAProject/disease.txt");
-        Reading.readFile("TAProject/disease.txt");
+        Reading.readFile("TAProject/medicine.txt");
 
-        System.out.println(DiseaseModel.getInstance().getModel().toString());
-        System.out.println(MedicineModel.getInstance().getModel().toString());
+        String[] userSymptoms = {"воспаление лимфоузлов","сиплый голос","боль в горле","температура"};
+
+//        System.out.println(DiseaseModel.getInstance().getModel().toString());
+//        System.out.println(MedicineModel.getInstance().getModel().toString());
+        WhichDisease whichDisease = new WhichDisease();
+        whichDisease.setSymptoms(userSymptoms);
+        whichDisease.whichDisease();
+        //System.out.println(whichDisease.whichDisease().toString());
     }
 }
