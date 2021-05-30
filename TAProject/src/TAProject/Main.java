637a47dababc2a1f357de889074ced58a1589f2c
Logvynskyy <shimon511bm@gmail.com>
2021-05-30 23:33:18 +0300

    Наконец-то получилось

--- TAProject/src/TAProject/Main.java
+++ TAProject/src/TAProject/Main.java
@@ -4,10 +4,7 @@ import TAProject.BD.DiseaseModel;
 import TAProject.BD.MedicineModel;
 import TAProject.Entities.Disease;
 import TAProject.Entities.Medicine;
-import TAProject.Methods.Reading;
-import TAProject.Methods.QuickSort;
-import TAProject.Methods.ShellSort;
-import TAProject.Methods.Sort;
+import TAProject.Methods.*;
 
 import java.util.Arrays;
 
@@ -19,6 +16,10 @@ public class Main {
         Reading.readFile("TAProject/disease.txt");
         System.out.println(DiseaseModel.getInstance().getModel().toString());
         System.out.println(MedicineModel.getInstance().getModel().toString());
+
+        String[] userSymptoms = new String[]{"воспаление лимфоузлов","сиплый голос","боль в горле","температура"};
+        WhichDisease whichDisease = new WhichDisease();
+        whichDisease.setSymptoms(userSymptoms);
 
 
 
